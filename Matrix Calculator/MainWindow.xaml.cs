795b98cb8c99ae5d75fb3a398495b1f5eb932cff
Diana <kozachenkodiana03@mail.ru>
2021-09-22 21:14:46 +0700

    Добавьте файлы проекта.

---
+++ Matrix Calculator/MainWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Matrix_Calculator
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Matrix m1;
+        public Matrix m2;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Fill_Click(object sender, RoutedEventArgs e)
+        {
+            var rand = new Random();
+            int[,] mas = new int[5, 5];
+
+            for (int i = 0; i < 5; ++i)
+            {
+                for (int p = 0; p < 5; ++p)
+                {
+                    var num = rand.Next(10);
+                    mas[i, p] = num;
+                }
+            }
+
+            m1 = new Matrix(5, 5, mas);
+            mas = new int[5, 5];
+            for (int i = 0; i < 5; ++i)
+            {
+                for (int p = 0; p < 5; ++p)
+                {
+                    var num = rand.Next(10);
+                    mas[i, p] = num;
+                }
+            }
+
+            
+
+            m2 = new Matrix(5, 5, mas);
+
+            string str = "";
+
+            for (int i = 0; i < 5; ++i)
+            {
+                for (int p = 0; p < 5; ++p)
+                {
+                    str += m1.nums[i, p].ToString() + " ";
+                }
+                str += '\n';
+            }
+
+            text1.Text = str;
+
+            str = "";
+            for (int i = 0; i < 5; ++i)
+            {
+                for (int p = 0; p < 5; ++p)
+                {
+                    str += m2.nums[i, p].ToString() + " ";
+                }
+                str += '\n';
+            }
+
+            text2.Text = str;
+        }
+
+        private void Action_Click(object sender, RoutedEventArgs e)
+        {
+            var additionMatrix = m1.multiplication(m2);
+
+            string str = "";
+
+            for (int i = 0; i < 5; ++i)
+            {
+                for (int p = 0; p < 5; ++p)
+                {
+                    str += additionMatrix.nums[i, p].ToString() + " ";
+                }
+                str += '\n';
+            }
+
+            text3.Text = str;
+
+
+            int det1 = m1.getDeterminant();
+            int det2 = m2.getDeterminant();
+
+            text4.Text = $"det1 {det1} \t det2 {det2}";
+        }
+    }
+}
---
+++ Matrix Calculator/Matrix.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Matrix_Calculator
+{
+    public class Matrix
+    {
+        public int rows { get; }
+        public int columns { get; }
+
+        public int[,] nums;
+
+        public Matrix()
+        {
+
+        }
+
+
+        public Matrix(int rows, int columns, int[,] nums)
+        {
+            if (rows > 0 && columns > 0)
+            {
+                this.rows = rows;
+                this.columns = columns;
+
+                this.nums = new int[rows, columns];
+
+                for (int i = 0; i < rows; ++i)
+                {
+                    for (int p = 0; p < columns; ++p)
+                    {
+                        this.nums[i, p] = nums[i, p];
+                    }
+                }
+            } else
+            {
+                throw new ArgumentException("One of arguments or both is unacceptable");
+            }
+
+        }
+
+
+        public Matrix addition(Matrix matrix2)
+        {
+            if (rows == matrix2.rows && columns == matrix2.columns)
+            {
+                int [,]additionMas = new int[rows, columns];
+
+                for (int i = 0; i < rows; ++i)
+                {
+                    for (int p = 0; p < columns; ++p)
+                    {
+                        additionMas[i, p] = nums[i, p] + matrix2.nums[i, p];
+                    }
+                }
+
+                return new Matrix(rows, columns, additionMas);
+            } else
+            {
+                throw new ArgumentException("Dimension of the matrices is different");
+            }
+        }
+        public Matrix substraction(Matrix matrix2)
+        {
+            if (rows == matrix2.rows && columns == matrix2.columns)
+            {
+                int[,] substractionMas = new int[rows, columns];
+
+                for (int i = 0; i < rows; ++i)
+                {
+                    for (int p = 0; p < columns; ++p)
+                    {
+                        substractionMas[i, p] = nums[i, p] - matrix2.nums[i, p];
+                    }
+                }
+
+                return new Matrix(rows, matrix2.columns, substractionMas);
+            }
+            else
+            {
+                throw new ArgumentException("Dimension of the matrices is different");
+            }
+        }
+        public Matrix multiplication(Matrix matrix2)
+        {
+            if (columns == matrix2.rows)
+            {
+                int[,] multiplicationMas = new int[rows, matrix2.columns];
+
+                for (int i = 0; i < rows; ++i)
+                {
+                    for (int p = 0; p < matrix2.columns; ++p)
+                    {
+                        int num = multiplicationRowOnColumn(columns, nums, matrix2.nums, i, p);
+
+                        multiplicationMas[i, p] = num;
+                    }
+                }
+
+                return new Matrix(rows, matrix2.columns, multiplicationMas);
+            }
+            else
+            {
+                throw new ArgumentException("Number of columns of one matrix is not equal to the number of rows of second matrix");
+            }
+        }
+
+        private int multiplicationRowOnColumn(int columns1, int[,] mas1, int[,] mas2, int indRow, int indColumn)
+        {
+            int result = 0;
+            for (int i = 0; i < columns1; ++i)
+            {
+                result += mas1[indRow, i] * mas2[i, indColumn];
+            }
+            return result;
+        }
+
+        public int getDeterminant()
+        {
+            if (rows == columns)
+            {
+                int order = getOrder();
+
+                if (order == 1)
+                {
+                    return nums[0,0];
+                } else if (order == 2)
+                {
+                    return nums[0, 0] * nums[1, 1] - nums[0, 1] * nums[1, 0]; 
+                } else if (order == 3)
+                {
+                    int mainDiagonal = nums[0, 0] * nums[1, 1] * nums[2, 2] + nums[0, 1] * nums[1, 2] * nums[2, 0] + nums[0, 2] * nums[1, 0] * nums[2, 1];
+                    int secondaryDiagonal = nums[0, 2] * nums[1, 1] * nums[2, 0] + nums[0, 1] * nums[1, 0] * nums[2, 2] + nums[0, 0] * nums[2, 1] * nums[1, 2];
+                    return mainDiagonal - secondaryDiagonal;
+                } else
+                {
+                    int sum = 0;
+
+                    for (int i = 0; i < rows; ++i)
+                    {
+                        int indexRow = i, indexColumn = 0;
+                        var newMatrix = getReducedMatrix(indexRow, indexColumn);
+                        int minor = newMatrix.getDeterminant();
+                        int algebraicComplement = Convert.ToInt32(Math.Pow(-1, indexRow + indexColumn)) * minor;
+
+                        sum += nums[indexRow, indexColumn] * algebraicComplement;
+                    }
+
+                    return sum;
+                }
+            } else
+            {
+                throw new ArgumentException("Number of columns is not equal to the number of rows");
+            }
+        }
+
+        private int getOrder()
+        {
+            return rows;
+        }
+
+        private Matrix getReducedMatrix(int indDeleteRow, int indDeleteColumn)
+        {
+            int newSize = rows - 1;
+            int[,] newMas = new int[newSize, newSize];
+
+            for (int i = 0; i < rows; ++i)
+            {
+                for (int p = 0; p < columns; ++p)
+                {
+                    if (i < indDeleteRow && p < indDeleteColumn)
+                    {
+                        newMas[i, p] = nums[i, p];
+                    } else if (i < indDeleteRow && p > indDeleteColumn)
+                    {
+                        newMas[i, p - 1] = nums[i, p];
+                    } else if (i > indDeleteRow && p < indDeleteColumn)
+                    {
+                        newMas[i - 1, p] = nums[i, p];
+                    } else if (i > indDeleteRow && p > indDeleteColumn)
+                    {
+                        newMas[i - 1, p - 1] = nums[i, p];
+                    }
+
+                }
+            }
+
+            var newMatrix = new Matrix(newSize, newSize, newMas);
+            return newMatrix;
+        }
+
+    }
+}
